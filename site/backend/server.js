bb94b7f755dd23a911974ed0f5864cd4cbb2d6fb
MNBRATHNAYAKE <140725915+MNBRATHNAYAKE@users.noreply.github.com>
2025-12-05 12:51:20 +0530

    Update server.js

--- site/backend/server.js
+++ site/backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://monitor1ng.netlify.app/',
 }));
 app.use(express.json());
 
